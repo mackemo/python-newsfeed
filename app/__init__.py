deccfce01f553af18e5aee3031d5ddbe7039fbb9
mackemo <mackenzielmoore14@gmail.com>
2024-09-12 10:12:26 -0400

    Completed signup route with error catching and added session

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from .routes import home, dashboard
+from .routes import home, dashboard, api
 from flask import Flask
 from app.db import init_db
 from app.utils import filters
@@ -18,6 +18,7 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  app.register_blueprint(api)
 
   app.jinja_env.filters['format_url'] = filters.format_url
   app.jinja_env.filters['format_date'] = filters.format_date
